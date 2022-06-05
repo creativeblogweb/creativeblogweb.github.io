e3a4b6e7a199b89e8c1e99b306066c0bbecdfe08
creativeblogweb <105432390+creativeblogweb@users.noreply.github.com>
2022-06-05 11:03:33 +0300

    Add files via upload

---
+++ uygulamatakvim.js
@@ -0,0 +1,24 @@
+    var tt=new Date();
+
+    var ay=new Array(12);
+    ay[0]="Ocak";
+    ay[1]="Şubat";
+    ay[2]="Mart";
+    ay[3]="Nisan";
+    ay[4]="Mayıs";
+    ay[5]="Haziran";
+    ay[6]="Temmuz";
+    ay[7]="Ağustos";
+    ay[8]="Eylül";
+    ay[9]="Ekim";
+    ay[10]="Kasım";
+    ay[11]="Aralık";
+    
+    var gun=new Array(7);
+    gun[0]="Pazar";
+    gun[1]="Pazartesi";
+    gun[2]="Salı";
+    gun[3]="Çarşamba";
+    gun[4]="Perşembe";
+    gun[5]="Cuma";
+    gun[6]="Cumartesi";
